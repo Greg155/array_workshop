3d87ec009376f8a4c0a0d9aec762d2296ed84d83
Greg155 <gregakhun@gmail.com>
2020-09-24 12:27:52 +0200

    workshop array complete

--- script.js
+++ script.js
@@ -7,14 +7,49 @@ console.log(listOfNumber);
 let reverseList = listOfNumber.reverse();
 console.log("Array reverse : " + reverseList);
 
-let firstLittleArray = [4,5,6,7,8];
+let firstLittleArray = [1,1,1,1,1,1];
 console.log("First little array is : " + firstLittleArray);
 
-let secondLittleArray = [4,5,6,7,8];
+let secondLittleArray = [2,3,4,5,6];
 console.log("Second little array is : " + secondLittleArray);
 
 let bigArray = [];
-for(let i = 0; i<firstLittleArray.length; i++){
-    bigArray[i] = (firstLittleArray[i] + secondLittleArray[i]);
+let bigger=-1;
+
+if(firstLittleArray.length > secondLittleArray.length){
+  bigger = firstLittleArray.length;
+}else{
+  bigger= secondLittleArray.length
 }
-console.log(bigArray);
+
+for(let i = 0; i<bigger; i++){
+    if(!firstLittleArray[i]){
+      bigArray[i] = secondLittleArray[i];
+    }else if(!secondLittleArray[i]){
+      bigArray[i] = firstLittleArray[i];
+    }else{
+      bigArray[i] = (firstLittleArray[i] + secondLittleArray[i]);
+    }
+  }
+console.log("Big array is " +bigArray);
+
+//////////////////////////////////
+let array1 = [1,2,3];
+let array2 = [11,12,13,14];
+let sum = [];
+let bigger, smaller 
+
+if(array1.length > array2.length){
+    bigger = array1;
+    smaller = array2;
+}
+else{
+    bigger = array2;
+    smaller = array1;
+}
+
+for(let i=0;i<bigger.length;i++){
+      i < smaller.length ? sum[i] = bigger[i] + smaller[i] : sum[i] = bigger[i]
+}
+console.log("add "+ sum)
+
