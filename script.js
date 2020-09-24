77ebcde41ba3bb915356e9dfda1b1cf35817e8cf
Greg155 <gregakhun@gmail.com>
2020-09-24 10:14:49 +0200

    training arrays manipulation

---
+++ script.js
@@ -0,0 +1,20 @@
+let listOfNumber = new Array(10);
+listOfNumber = [0,1,2,3,4,5,6,7,8,9];
+console.log("Array = " + listOfNumber);
+
+console.log(listOfNumber);
+
+let reverseList = listOfNumber.reverse();
+console.log("Array reverse : " + reverseList);
+
+let firstLittleArray = [4,5,6,7,8];
+console.log("First little array is : " + )
+let secondLittleArray = [4,5,6,7,8];
+
+let bigArray = [];
+
+for(let i = 0; i<littleArray.length; i++){
+    bigArray[i] = (firstLittleArray[i] + secondLittleArray[i]);
+
+}
+console.log(bigArray);
